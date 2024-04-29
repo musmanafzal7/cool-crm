0805752d0cc336fb96eb031adc8adae0a086cb4b
iman-zulfiqar298 <123621802+Iman-zulfiqar@users.noreply.github.com>
2024-04-29 05:19:22 -0600

    commit

--- src/assets/components/Tables/CRMTable.jsx
+++ src/assets/components/Tables/CRMTable.jsx
@@ -9,23 +9,26 @@ function CRMTable(props) {
   return (
     <>
       <div
-        className={`rounded-md border m-2 transition-all duration-[9s] ease-in-out flex-1`}
+        className={`rounded-md border  m-2 transition-all duration-[9s] ease-in-out flex-1`}
       >
         <div className="w-full sticky top-0 bg-blue-800 text-white flex justify-between p-2 ">
           <h1>CRM Search</h1>
           <button>Reset Filters</button>
         </div>
-        <Table
-          columns={props.columns}
-          dataSource={props.rows}
-          onChange={onChange}
-          pagination={{
-            pageSize: 8,
-          }}
-          showSorterTooltip={{
-            target: "sorter-icon",
-          }}
-        />
+        <div className="max-w-[1000px] w-full overflow-x-auto">
+          <Table
+            columns={props.columns}
+            dataSource={props.rows}
+            onChange={onChange}
+            pagination={{
+              pageSize: 8,
+            }}
+          
+            showSorterTooltip={{
+              target: "sorter-icon",
+            }}
+          />
+        </div>
       </div>
     </>
   );
