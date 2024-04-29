d3e59da4c9b14844c13d56d1653f11775260f1cd
iman-zulfiqar298 <123621802+Iman-zulfiqar@users.noreply.github.com>
2024-04-29 05:30:34 -0600

    commit

--- src/assets/components/Tables/CRMTable.jsx
+++ src/assets/components/Tables/CRMTable.jsx
@@ -1,11 +1,17 @@
 import PropTypes from "prop-types";
 
 import { Table } from "antd";
+import { useState } from "react";
+
 
 const onChange = (pagination, filters, sorter, extra) => {
   console.log("params", pagination, filters, sorter, extra);
 };
 function CRMTable(props) {
+ const [filteredInfo, setFilteredInfo] = useState({});
+ const clearFilters = () => {
+    setFilteredInfo({});
+  };
   return (
     <>
       <div
@@ -13,9 +19,9 @@ function CRMTable(props) {
       >
         <div className="w-full sticky top-0 bg-blue-800 text-white flex justify-between p-2 ">
           <h1>CRM Search</h1>
-          <button>Reset Filters</button>
+          <button onClick={clearFilters}>Reset Filters</button>
         </div>
-        <div className="max-w-[1000px] w-full overflow-x-auto">
+        <div className="max-w-[1590px] w-full overflow-x-auto">
           <Table
             columns={props.columns}
             dataSource={props.rows}
